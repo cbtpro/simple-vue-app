ee51b45a7cf65848c427539f9649d01db02b21d9
chenbitao <cbtpro@gmail.com>
2025-07-23 22:58:32 +0800

    feat: 更新脚手架，支持部署到github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  devServer: {
-    static: './dist',
-    port: 3000,
-    hot: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  }
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    mode: isProd ? 'production' : 'development',
+    devServer: {
+      static: './dist',
+      port: 3000,
+      hot: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      ...(isProd ? { publicPath: '/simple-vue-app' } : {}),
+    }
+  };
 };
